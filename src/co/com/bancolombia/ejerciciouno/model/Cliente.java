adf1dba388304537a36be8081748c7325780975d
Bryan <73038229+bryanumana@users.noreply.github.com>
2022-02-02 12:05:12 -0500

    Herencia

--- src/co/com/bancolombia/ejerciciouno/model/Cliente.java
+++ src/co/com/bancolombia/ejerciciouno/model/Cliente.java
@@ -1,60 +1,18 @@
 package co.com.bancolombia.ejerciciouno.model;
 
-public class Cliente {
+public class Cliente extends Persona{
 
     private int cedulaCliente;
     private String nombreCliente;
     private String primerApellidoCliente;
     private String segundoApellidoCliente;
 
-    private String estadoCivil;
-    private int cantidadHijos;
-    private int cantidadHermanos;
-    private String nombreDelPadre;
-    private String nombreDeLaMadre;
+
 
     public Cliente() {
     }
 
-    public String getEstadoCivil() {
-        return estadoCivil;
-    }
 
-    public void setEstadoCivil(String estadoCivil) {
-        this.estadoCivil = estadoCivil;
-    }
-
-    public int getCantidadHijos() {
-        return cantidadHijos;
-    }
-
-    public void setCantidadHijos(int cantidadHijos) {
-        this.cantidadHijos = cantidadHijos;
-    }
-
-    public int getCantidadHermanos() {
-        return cantidadHermanos;
-    }
-
-    public void setCantidadHermanos(int cantidadHermanos) {
-        this.cantidadHermanos = cantidadHermanos;
-    }
-
-    public String getNombreDelPadre() {
-        return nombreDelPadre;
-    }
-
-    public void setNombreDelPadre(String nombreDelPadre) {
-        this.nombreDelPadre = nombreDelPadre;
-    }
-
-    public String getNombreDeLaMadre() {
-        return nombreDeLaMadre;
-    }
-
-    public void setNombreDeLaMadre(String nombreDeLaMadre) {
-        this.nombreDeLaMadre = nombreDeLaMadre;
-    }
 
     public int getCedulaCliente() {
         return cedulaCliente;
@@ -89,19 +47,21 @@ public class Cliente {
     }
 
 
+
+    public void imprimirInformacionCliente(){
+        System.out.println("Número de cedula:" + this.getCedulaCliente());
+        System.out.println("Nombre: " + this.getNombreCliente());
+        System.out.println("Primer Apellido: " + this.getPrimerApellidoCliente());
+        System.out.println("Segundo Apellido: " + this.getSegundoApellidoCliente());
+        System.out.println("");
+    }
+
     public void imprimirInformacionFamiliar(){
         System.out.println("Estado civil:" + this.getEstadoCivil());
         System.out.println("Número de hijos:" + this.getCantidadHijos());
         System.out.println("Número de hermanos: " + this.getCantidadHermanos());
         System.out.println("Nombre del padre: " + this.getNombreDelPadre());
         System.out.println("Nombre de la madre: " + this.getNombreDeLaMadre());
-        System.out.println("");
-    }
-    public void imprimirInformacionCliente(){
-        System.out.println("Número de cedula:" + this.getCedulaCliente());
-        System.out.println("Nombre: " + this.getNombreCliente());
-        System.out.println("Primer Apellido: " + this.getPrimerApellidoCliente());
-        System.out.println("Segundo Apellido: " + this.getSegundoApellidoCliente());
         System.out.println("");
     }
 
---
+++ src/co/com/bancolombia/ejerciciouno/model/Persona.java
@@ -0,0 +1,51 @@
+package co.com.bancolombia.ejerciciouno.model;
+
+public class Persona {
+    private String estadoCivil;
+    private int cantidadHijos;
+    private int cantidadHermanos;
+    private String nombreDelPadre;
+    private String nombreDeLaMadre;
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public int getCantidadHijos() {
+        return cantidadHijos;
+    }
+
+    public void setCantidadHijos(int cantidadHijos) {
+        this.cantidadHijos = cantidadHijos;
+    }
+
+    public int getCantidadHermanos() {
+        return cantidadHermanos;
+    }
+
+    public void setCantidadHermanos(int cantidadHermanos) {
+        this.cantidadHermanos = cantidadHermanos;
+    }
+
+    public String getNombreDelPadre() {
+        return nombreDelPadre;
+    }
+
+    public void setNombreDelPadre(String nombreDelPadre) {
+        this.nombreDelPadre = nombreDelPadre;
+    }
+
+    public String getNombreDeLaMadre() {
+        return nombreDeLaMadre;
+    }
+
+    public void setNombreDeLaMadre(String nombreDeLaMadre) {
+        this.nombreDeLaMadre = nombreDeLaMadre;
+    }
+
+
+}
